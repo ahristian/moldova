7373f5dff1f947e66f6be22d17a87294b512e187
ahristian <hristian.andrei@gmail.com>
2020-08-25 08:09:24 -0600

    added routes

--- backend/src/routes/index.routes.ts
+++ backend/src/routes/index.routes.ts
@@ -1,7 +1,7 @@
 import {Router} from "express"
 import {indexController} from '../controllers/index.controller'
 
-export const indexRoutes = Router()
+export const IndexRoutes = Router()
 
-indexRoutes.route("/")
+IndexRoutes.route("/")
     .get(indexController)
