42bf2f38e1b5b90eec9f60f7181031df5a0996db
ahristian <hristian.andrei@gmail.com>
2020-08-29 16:31:06 -0600

    add select destination

--- backend/utils/destination/selectAllDestinations.ts
+++ backend/utils/destination/selectAllDestinations.ts
@@ -1,15 +1,13 @@
-import {Destination} from "../interfaces/Destination";
+
 import {connect} from "../../src/database";
-import {Profile} from "../interfaces/Profile";
-import {Status} from "../interfaces/Status";
 
 export async function selectAllDestinations() {
     try {
         const mySqlConnection = await connect()
-        const mySqlQuery = "SELECT destinationContact, destinationDescription, destinationLocation, destinationTitle, destinationImageUrl FROM destination";
+        const mySqlQuery = "SELECT  BIN_TO_UUID(destinationId) AS destinationId, destinationContact, destinationDescription, destinationLocation, destinationTitle, destinationImageUrl FROM destination";
         const [rows] = await mySqlConnection.execute(mySqlQuery)
         return rows;
-    } catch (error) {
-        console.log(error)
-    }
+} catch (error) {
+    console.log(error)
+}
 }
