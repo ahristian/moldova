18f9b054ec6c209c9ac058ae7eaf6b7697fa5040
ahristian <hristian.andrei@gmail.com>
2020-08-29 16:28:52 -0600

    added database

--- backend/src/database.ts
+++ backend/src/database.ts
@@ -1,12 +1,12 @@
 import { createPool, Pool } from 'mysql2/promise'
+import  mysql from 'mysql'
 
 require('dotenv').config()
 
-// Database connection. Can use config from .env file (based on example.env) or the defaults
 
 export async function connect (): Promise<Pool> {
     const connection = await createPool({
-        host: process.env.HOST,
+        host: process.env.MYSQL_HOST,
         user: process.env.MYSQL_USER,
         password: process.env.MYSQL_PASSWORD,
         database: process.env.MYSQL_DATABASE,
@@ -15,3 +15,32 @@ export async function connect (): Promise<Pool> {
     })
     return connection
 }
+let connection
+
+export const  dbConfig = {
+    host: "localhost",
+    port: 3306,
+    user:"root",
+    password: "",
+    database:"testuser"
+}
+
+export async function handleConnection(){
+    let connection = mysql.createConnection(dbConfig);
+    connection.connect(err => {
+        if (err) {
+            console.log('Error creating connection');
+            setTimeout(handleConnection, 2000)
+        }
+        console.log('Database is ready to roll');
+    })
+    connection.on('error', (err: any) => {
+        if (err.code === 'PROTOCOL_CONNECTION_LOST') {
+            handleConnection();
+        } else {
+            throw err;
+        }
+    })
+}
+
+
