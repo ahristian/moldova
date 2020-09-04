35c176dff5e3018817033abae12436619fbb5bfb
ahristian <hristian.andrei@gmail.com>
2020-09-04 00:11:22 -0600

    added redux elements

--- frontend/src/ui/DestinationCard.js
+++ frontend/src/ui/DestinationCard.js
@@ -1,24 +1,24 @@
-import React, { useEffect } from 'react'
-import Popup from "reactjs-popup"
+import React from 'react'
 import Card from 'react-bootstrap/Card'
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
 
 export const DestinationCard = (props) => {
   const {destination} = props
   return (
     <>
+
       <a href={destination.destinationId}>
-      <Card className="destinationCardElement mx-3 my-3" style={{width: '25rem'}} >
+      <Card className="destinationCardElement mx-3 my-3"  >
         <Card.Body>
+          <img src={destination.destinationImage}  alt={destination.destinationTitle}/>
           <Card.Title className="text-center">
             {destination.destinationTitle}
           </Card.Title>
-          <Card.Text>
-            <p>{destination.destinationDescription}</p>
-          </Card.Text>
         </Card.Body>
       </Card>
       </a>
-
     </>
   )
 }
--- frontend/src/ui/DestinationElement.js
+++ frontend/src/ui/DestinationElement.js
@@ -1,17 +1,24 @@
 import React, { useEffect } from 'react'
 import Popup from "reactjs-popup"
 import Card from 'react-bootstrap/Card'
+import Carousel from 'react-bootstrap/Carousel'
+
 
 export const  DestinationElement= (props) => {
   const {destinationPhoto} = props
   return (
     <>
-        <Card className="destinationCardElement mx-2 my-5" style={{width: '14rem'}} >
-          <Card.Body>
-               <img src={`${destinationPhoto.destinationPhotoUrl}`}
-                alt={destinationPhoto.destinationTitle} className="img-fluid" />
-          </Card.Body>
-        </Card>
+      <div className="destinationEach mx-1 my-3">
+
+      <Popup
+        trigger={<a href="#">
+          <img className="destinationEachImage"  src={destinationPhoto.destinationPhotoUrl}
+                                  alt={destinationPhoto.destinationTitle}/></a> }
+        modal closeOnDocumentClick>
+        <img className="image d-block w-100" src={destinationPhoto.destinationPhotoUrl}  alt={destinationPhoto.destinationTitle}/>
+      </Popup>
+
+        </div>
 
     </>
   )
