d39a248bab5d7d9aa10096dfedaac29031318b78
ahristian <hristian.andrei@gmail.com>
2020-09-15 10:59:02 -0600

    fixed card sized padding

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -6,7 +6,6 @@ import { BrowserRouter } from "react-router-dom";
 import { Route, Switch } from "react-router";
 import { Traditions } from "./pages/Traditions";
 import { Food } from "./pages/Food";
-import { DestinationsPage } from "./pages/Destinations";
 import { SinglePage } from './pages/SinglePage'
 import { Home } from "./pages/Home";
 import { FooterElement } from './shared/components/FooterElement'
@@ -14,6 +13,7 @@ import { NavigationBar } from './shared/components/NavigationBar'
 import {configureStore} from '@reduxjs/toolkit'
 import reducer from './store'
 import {Provider} from 'react-redux'
+import { AboutMoldova } from './pages/AboutMoldova'
 
 const store = configureStore({ reducer})
 
@@ -23,9 +23,9 @@ const Routing = (store) => (
     <NavigationBar/>
     <BrowserRouter>
       <Switch>
+        <Route exact path="/aboutMoldova" component={AboutMoldova}/>
         <Route exact path="/traditions" component={Traditions}/>
         <Route exact path="/foods" component={Food}/>
-        <Route exact path="/destinations" component={DestinationsPage}/>
         <Route exact path="/destinations/:destinationId" component={SinglePage}  destinationId=":destinationId"/>
         <Route component={Home}/>
       </Switch>
