120595db2c3bf01ec49c9d77d4ae4d3e80731a2e
ahristian <hristian.andrei@gmail.com>
2020-09-15 16:22:57 -0600

    added profile

--- sql/moldova.sql
+++ sql/moldova.sql
@@ -15,7 +15,7 @@ CREATE TABLE profile (
                              PRIMARY KEY(profileId)
 );
 
-
+INSERT INTO profile (profileId, profileActivationToken, profileEmail, profileHash, profileUserName) VALUES (UUID_TO_BIN('b85b870a-e099-4bf0-b7c5-f0e311e0d862'), '86cf855e0d46e2435eda674df725b6ba', 'hristian@gmail.com', '$argon2id$v=19$m=65536,t=3,p=1$/z0bIUc60QkXCpvEcJlhjA$zE4VS9fm//xpYAP77yftEGG1NeYys0EsTK7oa7ce7ff', 'TestUser');
 CREATE TABLE destination (
                        destinationId BINARY(16) NOT NULL,
                        destinationContact VARCHAR(550) NOT NULL,
@@ -50,16 +50,12 @@ CREATE TABLE destinationPhoto (
 );
 CREATE TABLE `save` (
                         saveDestinationId BINARY(16) NOT NULL,
-                        saveFoodId BINARY(16) NOT NULL,
                         saveProfileId BINARY(16) NOT NULL,
-                        saveTraditionId BINARY(16) NOT NULL,
                         INDEX(saveDestinationId),
-                        INDEX(saveFoodId),
                         INDEX(saveProfileId),
-                        INDEX(saveTraditionId),
                         FOREIGN KEY(saveDestinationId) REFERENCES destination(destinationId),
-                        FOREIGN KEY(saveFoodId) REFERENCES food(foodId),
-                        FOREIGN KEY(saveProfileId) REFERENCES tradition(traditionId),
-                        FOREIGN KEY(saveTraditionId) REFERENCES profile(profileId),
-                        PRIMARY KEY(saveDestinationId, saveFoodId, saveProfileId, saveTraditionId)
+                        FOREIGN KEY(saveProfileId) REFERENCES profile(profileId),
+                        PRIMARY KEY(saveDestinationId, saveProfileId)
 );
+INSERT INTO `save` (saveDestinationId, saveProfileId) VALUES (UUID_TO_BIN('1050a0aa-b318-437e-aec1-03eb0022eb2d'), UUID_TO_BIN('b85b870a-e099-4bf0-b7c5-f0e311e0d862'));
+
