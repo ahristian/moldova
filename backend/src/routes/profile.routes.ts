32fe15622737d87a7e84f21cb42f48d0b56e3bef
ahristian <hristian.andrei@gmail.com>
2020-09-15 16:22:25 -0600

    added profile

--- backend/src/routes/profile.routes.ts
+++ backend/src/routes/profile.routes.ts
@@ -1,16 +1,12 @@
 
 import { Router } from 'express';
-import {
-    getAllDestinationsController,
-    selectAllDestinationsByDestinationIdController
-} from '../controllers/destination.controller';
+import {getProfileByIdController
+} from '../controllers/profile.controller';
 
 
 const router = Router();
 
-
 // Every new route is instantiated below. It will include the controller name and the type of action (get, post, delete, put, patch)
-router.route('/:destinationId').get(selectAllDestinationsByDestinationIdController);
-router.route('/').get(getAllDestinationsController);
+router.route('/:profileId').get(getProfileByIdController);
 
 export default router;
