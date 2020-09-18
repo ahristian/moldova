e8e0495655a99983c683aa1f63ba9df13bcbab29
ahristian <hristian.andrei@gmail.com>
2020-09-17 20:14:57 -0600

    adding profile

--- backend/src/routes/destination.routes.ts
+++ backend/src/routes/destination.routes.ts
@@ -10,6 +10,7 @@ const router = Router();
 
 
 // Every new route is instantiated below. It will include the controller name and the type of action (get, post, delete, put, patch)
+
 router.route('/:destinationId').get(selectAllDestinationsByDestinationIdController);
 router.route('/').get(getAllDestinationsController);
 
--- backend/src/routes/profile.routes.ts
+++ backend/src/routes/profile.routes.ts
@@ -1,12 +1,21 @@
 
 import { Router } from 'express';
-import {getProfileByIdController
-} from '../controllers/profile.controller';
-
+import {getDestinationByProfileIdController, getProfileByIdController} from '../controllers/profile.controller';
+import {asyncValidatorController} from "../controllers/asyncValidator.controller";
+import {check} from "express-validator";
 
 const router = Router();
 
-// Every new route is instantiated below. It will include the controller name and the type of action (get, post, delete, put, patch)
-router.route('/:profileId').get(getProfileByIdController);
+router.route("/:profileId")
+    .get(
+        asyncValidatorController([
+            check("profileId", "please provide a valid profileId").isUUID()
+        ]), getProfileByIdController)
+
+router.route("/destination/:profileId")
+    .get(
+        asyncValidatorController([
+            check("profileId", "please provide a valid profileId").isUUID()
+        ]), getDestinationByProfileIdController)
 
 export default router;
