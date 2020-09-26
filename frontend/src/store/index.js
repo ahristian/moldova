562789c4b814b4fb0df57d7606d4e4277c55a249
ahristian <hristian.andrei@gmail.com>
2020-09-25 20:21:28 -0600

    added confirmation

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -5,7 +5,8 @@ import foods from './food'
 import profile from './profile'
 import destinationPhotos from './destinationPhoto'
 import save from './saved'
-import auth from "./auth"
-
+import auth from "./auth"/*
+import profileActivationToken from "./activation"
+*/
 
 export default combineReducers({traditions, profile, destinations, foods, destinationPhotos, save, auth})
